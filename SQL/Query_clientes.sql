f99e77bb5e9281f83d689ebf83a27bc090154147
CatherineCabreraTT24 <catherine.eliana@tripleten-team.com>
2025-08-28 18:59:34 -0500

    Query

--- SQL/Query_clientes.sql
+++ SQL/Query_clientes.sql
@@ -1,13 +1,13 @@
 -- Crear tabla Employee
 CREATE TABLE Clientes (
     ID INTEGER PRIMARY KEY AUTOINCREMENT,
-    Name TEXT NOT NULL,
-    Age INTEGER,
-    Country TEXT
+    nombre TEXT NOT NULL,
+    edad INTEGER,
+    pais TEXT
 );
 
 -- Insertar algunos registros de ejemplo
-INSERT INTO Clientes (Name, Age, Country) VALUES
+INSERT INTO Clientes (nombre, edad, pais) VALUES
 ('Juan Pérez', 28, 'Colombia'),
 ('Ana Gómez', 22, 'México'),
 ('José Ramírez', 35, 'Colombia'),
